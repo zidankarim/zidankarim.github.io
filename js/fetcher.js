912e54ddcc8681a0218428c1ff1091d992ee379c
paijolongor <zidankarim5@gmail.com>
2020-09-29 02:41:22 +0700

    update 1

---
+++ js/fetcher.js
@@ -0,0 +1,14 @@
+var keyword = document.getElementById('keyword');
+var container = document.getElementById('container');
+
+
+keyword.addEventListener('keyup', function(){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		container.innerHTML = (xhr.responseText);
+	}}
+
+	xhr.open('GET', '/alumni/search?q='+keyword.value, true);
+	xhr.send()
+});
---
+++ js/timeline.js
@@ -0,0 +1,24 @@
+
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom-100 <= (window.innerHeight || document.documentElement.clientHeight) &&
+    //         ^^^^ LIFEHACK :V
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
